289e7aac36e83b688ff169877d461f0238abb097
Caio1825 <165043814+Caio1825@users.noreply.github.com>
2026-03-03 12:04:56 -0300

    Add multiple functionalities including multiplication, average, and login

--- questions.js
+++ questions.js
@@ -32,3 +32,108 @@ if(operacao == 1){
     let multiplicacao = numero * numero2;
     console.log("A multiplicação do primeiro " + numero + (" mais o segundo ") + numero2 + " vai dar " + multiplicacao);
 }
+
+Tabuada
+let numero = parseInt(prompt("INSIRA UM NUMERO: "));
+for(let i=0;i<=10;i++){
+    let resultado = numero * i;
+    console.log(numero + "x" + i + "= " + resultado );
+}
+
+Media
+let nota = parseInt(prompt("INSIRA A PRIMEIRA NOTA: "));
+let nota2 = parseInt(prompt("INSIRA A SEGUNDA NOTA: "));
+let nota3 = parseInt(prompt("INSIRA A TERCEIRA NOTA: "));
+let media = (nota + nota2 + nota3) /3
+ if(media >= 7){
+     console.log("Parabéns, você passou!! ");
+ }else if(media >=5 && media < 7){
+     console.log("Você está de recuperação");
+ }else{
+     console.log("Meus parabéns, você repitiu de ano !!")
+ }
+
+Contagem 
+let numero = parseInt(prompt("Digite um número inteiro positivo:"));
+
+if (numero > 0) {
+    for (let i = 1; i <= numero; i++) {
+        console.log(i);
+    }
+} else {
+    console.log("Por favor, digite um número inteiro positivo.");
+}
+Soma até digitar 0
+let soma = 0;
+let numero;
+
+do {
+    numero = parseFloat(prompt("Digite um número (0 para parar):"));
+    soma += numero;
+} while (numero !== 0);
+
+console.log("Soma total:", soma);
+
+Contador de Numeros Positivos
+let positivos = 0;
+
+for (let i = 1; i <= 5; i++) {
+    let numero = parseFloat(prompt("Digite o " + i + "º número:"));
+    
+    if (numero > 0) {
+        positivos++;
+    }
+}
+
+console.log("Quantidade de números positivos:", positivos);
+
+Contador de Numeros Positivos
+let positivos = 0;
+
+for (let i = 1; i <= 5; i++) {
+    let numero = parseFloat(prompt("Digite o " + i + "º número:"));
+    
+    if (numero > 0) {
+        positivos++;
+    }
+}
+
+console.log("Quantidade de números positivos:", positivos);
+
+Sistema de Login
+let usuarioCorreto = "admin";
+let senhaCorreta = "1234";
+let tentativas = 0;
+let acesso = false;
+
+while (tentativas < 3) {
+    let usuario = prompt("Digite o usuário:");
+    let senha = prompt("Digite a senha:");
+
+    if (usuario === usuarioCorreto && senha === senhaCorreta) {
+        acesso = true;
+        break;
+    } else {
+        tentativas++;
+        console.log("Usuário ou senha incorretos.");
+    }
+}
+
+if (acesso) {
+    console.log("Login realizado com sucesso!");
+} else {
+    console.log("Acesso bloqueado. Número máximo de tentativas atingido.");
+}
+
+Calculo Fatorial
+let numero = parseInt(prompt("Digite um número inteiro positivo:"));
+let fatorial = 1;
+
+if (numero >= 0) {
+    for (let i = 1; i <= numero; i++) {
+        fatorial *= i;
+    }
+    console.log("Fatorial de " + numero + " é: " + fatorial);
+} else {
+    console.log("Digite um número válido.");
+}
